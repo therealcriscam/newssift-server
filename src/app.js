38fe3af42e5d262710348fd1df76f9518dc2c780
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-05-31 14:23:21 -0700

    deploy

---
+++ src/app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config');
+
+const app = express();
+
+const morganOption = (process.env.NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption));
+
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
+
+app.use(helmet());
+
+app.get('/api/', (req, res) => {
+  res
+    .send('Hello, world!')
+})
+
+app.use(function errorHandler(error, req, res, next) {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    console.error(error)
+    response = { message: error.message, error }
+  }
+  res.status(500).json(response)
+})
+
+module.exports = app
---
+++ test/app.spec.js
@@ -0,0 +1,9 @@
+const app = require('../src/app');
+
+describe('App', () => {
+  it('GET /api/ responds with 200 containing "Hello, world!"', () => {
+    return supertest(app)
+      .get('/api/')
+      .expect(200, "Hello, world!")
+  });
+});
