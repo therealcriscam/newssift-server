17de0ba5ebf24cb8448c8f7de6d860f4b60dac83
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-06-07 11:59:24 -0700

    db

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DB_URL: process.env.DB_URL || 'postgresql://newssift@localhost/newssift',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://newssift@localhost/newssift',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
 }
